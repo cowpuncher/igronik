9ce25e93aa349327e89403410f2471c8b6245463
WebDev TVlean <tvleanjohnny@gmail.com>
2022-11-02 16:12:40 +0300

    add full page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     
-    let activedSlider = (newSlider) => {
+    let activedSlider = (slider) => {
         newSlider = new Swiper('.clientsItem', {
             loop: true,   
             slidesPerView: 5, 
@@ -8,6 +8,26 @@ document.addEventListener("DOMContentLoaded", function() {
               nextEl: '.arrow-right',
               prevEl: '.arrow-left',
             },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                  slidesPerView: 2,
+                  spaceBetween: 20
+                },
+                // when window width is >= 640px
+                640: {
+                  slidesPerView: 3,
+                  spaceBetween: 40
+                },
+                993: {
+                  slidesPerView: 4,
+                  spaceBetween: 40
+                },
+                1200: {
+                  slidesPerView: 5,
+                  spaceBetween: 40
+                }
+            }
         });
     }
     const clientsItem = document.querySelectorAll('.clientsItem');
@@ -24,6 +44,29 @@ document.addEventListener("DOMContentLoaded", function() {
             });            
             e.target.classList.add('active');
         });
+    });
+
+    const teamSlider = new Swiper('.teamSlider', {
+        loop: true,   
+        slidesPerView: 4, 
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.arrow-right',
+          prevEl: '.arrow-left',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+            },
+            // when window width is >= 640px
+            568: {
+              slidesPerView: 3,
+            },
+            993: {
+              slidesPerView: 4,
+            },
+        }
     });
 
 });
